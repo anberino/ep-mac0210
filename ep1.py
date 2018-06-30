d8b195324af5696e2087a91d107ff4fd35ef4d23
Anb <gabriel.massukado@hotmail.com>
2018-06-30 15:25:11 -0300

    Done? Falta pegar da entrada

--- ep1.py
+++ ep1.py
@@ -10,9 +10,9 @@ import functools
 
 def test(n, min, max):
     #gera o valor certo
-    w = np.random.rand(n)
-    w-= 0.5 
-    s = spline(w, min, max)
+    ws = np.random.rand(n)
+    ws-= 0.5 
+    s = spline(ws, min, max)
     t = np.arange(min, max, 0.001)
     m = len(t)
     plt.plot(t, s(t))
@@ -31,18 +31,27 @@ def test(n, min, max):
     arraytemp = np.full(len(t), 1)
     #print(arraytemp) #test print
     temp = spline(arraytemp, min, max)
-    B = np.zeros([n, m]) #deve ter um jeito melhor de fazer isso
+    Bt = np.zeros([n, m]) #deve ter um jeito melhor de fazer isso
     for i in range(n):
         for j in range(m):
-            B[i][j] = temp.beta_j(i, t[j])
+            Bt[i][j] = temp.beta_j(i, t[j])
     #print(B) #test print
     
     #calcula a matrix M1, a M2 e a b ((M1+lM2)w = b) para acharmos w
-    Bt = np.matrix.transpose(B)
+    B = np.matrix.transpose(Bt)
     M1 = np.matmul(Bt, B)
     M2 = matrix_m2(n)
+    b = np.matmul(Bt, ynoise)
+    l = 1 #completamente arbitrário, segundo Mascarenhas
+    M = M1 + l*M2
     
-    #b = np.matmul(Bt, ynoise) #falando que não bate os números, tem algo errado ali em cima
+    #resolve o sistema 
+    w = np.linalg.solve(M, b)
+    
+    #plota a nova spline
+    aprox = spline(w)
+    plt.plot(t, aprox(t))
+    plt.show()
     
 
 test(10, 0, 1)
