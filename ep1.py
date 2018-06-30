2bbd35bcac8b6dc0566d341847d213160d8d6a4b
henrique cerquinho <henriquecerquinho@gmail.com>
2018-06-30 15:29:26 -0300

    merge fix

--- ep1.py
+++ ep1.py
@@ -10,15 +10,9 @@ import functools
 
 def test(n, min, max):
     #gera o valor certo
-<<<<<<< HEAD
     ws = np.random.rand(n)
-    ws-= 0.5 
+    ws-= 0.5
     s = spline(ws, min, max)
-=======
-    w = np.random.rand(n)
-    w-= 0.5
-    s = spline(w, min, max)
->>>>>>> 6872466a6549e5fd799a2dd79935faf0db47df33
     t = np.arange(min, max, 0.001)
     m = len(t)
     plt.plot(t, s(t), 'r')
@@ -46,23 +40,16 @@ def test(n, min, max):
     B = np.matrix.transpose(Bt)
     M1 = np.matmul(Bt, B)
     M2 = matrix_m2(n)
-<<<<<<< HEAD
     b = np.matmul(Bt, ynoise)
     l = 1 #completamente arbitrário, segundo Mascarenhas
     M = M1 + l*M2
-    
-    #resolve o sistema 
+
+    #resolve o sistema
     w = np.linalg.solve(M, b)
-    
+
     #plota a nova spline
     aprox = spline(w)
     plt.plot(t, aprox(t))
     plt.show()
-    
-=======
-
-    #b = np.matmul(Bt, ynoise) #falando que não bate os números, tem algo errado ali em cima
-
->>>>>>> 6872466a6549e5fd799a2dd79935faf0db47df33
 
 test(10, 0, 1)
