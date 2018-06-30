6872466a6549e5fd799a2dd79935faf0db47df33
henrique cerquinho <henriquecerquinho@gmail.com>
2018-06-30 15:23:24 -0300

    muitas coisas

--- ep1.py
+++ ep1.py
@@ -1,5 +1,5 @@
 # Gabriel Sarti Massukado - NUSP 10284177
-# Henrique Cerquinho - NUSP 
+# Henrique Cerquinho - NUSP 9793700
 
 from splines import *
 import numpy as np
@@ -11,14 +11,14 @@ import functools
 def test(n, min, max):
     #gera o valor certo
     w = np.random.rand(n)
-    w-= 0.5 
+    w-= 0.5
     s = spline(w, min, max)
     t = np.arange(min, max, 0.001)
     m = len(t)
     plt.plot(t, s(t))
     plt.show()
     y = s(t) #esse é o spline de vdd, eu acho
-    
+
     #gera o valor com ruído baseado em s
     noise = np.random.rand(m)
     noise-= 0.5
@@ -26,7 +26,7 @@ def test(n, min, max):
     ynoise =  y + noise
     plt.plot(t, ynoise)
     plt.show()
-    
+
     #cria uma spline temporária pra pegar os betajs
     arraytemp = np.full(len(t), 1)
     #print(arraytemp) #test print
@@ -36,13 +36,13 @@ def test(n, min, max):
         for j in range(m):
             B[i][j] = temp.beta_j(i, t[j])
     #print(B) #test print
-    
+
     #calcula a matrix M1, a M2 e a b ((M1+lM2)w = b) para acharmos w
     Bt = np.matrix.transpose(B)
     M1 = np.matmul(Bt, B)
     M2 = matrix_m2(n)
-    
+
     #b = np.matmul(Bt, ynoise) #falando que não bate os números, tem algo errado ali em cima
-    
+
 
 test(10, 0, 1)
