b004d6ff3ad39c2099980736658b6d52dcd0950d
Anb <gabriel.massukado@hotmail.com>
2018-06-29 22:08:43 -0300

    Gera valores normais e com ruído

--- ep1.py
+++ ep1.py
@@ -5,11 +5,23 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import functools
 
-def test():
-    x = np.random.rand(10)
-    print("oi")
-    s = spline(x)
+def test(n):
+    #gera o valor certo
+    w = np.random.rand(n)
+    w-= 0.5 
+    s = spline(w)
     t = np.arange(0, 1, 0.001)
     plt.plot(t, s(t))
+    plt.show()
+    y = s(t)
+    
+    #gera o valor com ruído
+    n = np.random.rand(len(t))
+    n-= 0.5
+    n*= 100
+    ynoise =  y + n
+    plt.plot(t, ynoise)
+    plt.show()
+    
 
-test()
+test(10)
